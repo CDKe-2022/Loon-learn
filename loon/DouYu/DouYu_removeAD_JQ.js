e52dbef4e46ee413df925b9cb9d8de931018e6bf
CDKe-2022 <104467212+CDKe-2022@users.noreply.github.com>
2024-11-12 11:05:07 +0800

    更新 DouYu_removeAD_JQ.js

--- loon/DouYu/DouYu_removeAD_JQ.js
+++ loon/DouYu/DouYu_removeAD_JQ.js
@@ -1,29 +1,31 @@
-# 首先检查根对象是否包含 `data` 字段
-if .data? then
+# 获取请求的URL
+def url: $request.url;
 
-    # 如果 `data` 对象包含 `rec_cont` 字段，则过滤其中的广告内容
-    if .data.rec_cont? then
-        # 通过 `map` 函数遍历 `rec_cont` 数组中的每个元素
-        # `select(.ad | not)` 选择没有 `ad` 字段的项，过滤掉含有广告的项
-        .data.rec_cont |= map(select(.ad | not))
-    end;
+# 定义一个函数，用于过滤掉广告内容
+def removeAds: map(select(.ad | not));
 
-    # 如果 `data` 对象包含 `rec_card` 字段，则继续处理每个 `rec_card` 项
-    if .data.rec_card? then
-        # 使用 `map` 遍历 `rec_card` 数组中的每个元素
-        .data.rec_card |= map(
-            # 针对每个 `rec_card` 项，检查是否包含 `card_banner` 字段
-            if .card_banner? then
-                # 对 `card_banner` 数组应用相同的广告过滤逻辑
-                .card_banner |= map(select(.ad | not))
-            else
-                # 如果没有 `card_banner` 字段，则保持原样
-                .
-            end
-        )
-    end
+# 尝试解析响应的JSON数据并进行处理
+if url | contains("/mgapi/livenc/home/getRecV3") then
+    .data |= (
+        if .rec_cont then
+            .rec_cont = removeAds(.rec_cont)
+        else
+            .
+        end
+        |
+        if .rec_card then
+            .rec_card |= map(
+                if .card_banner then
+                    .card_banner = removeAds(.card_banner)
+                else
+                    .
+                end
+            )
+        else
+            .
+        end
+    )
+end;
 
-else
-    # 如果根对象没有 `data` 字段，则不进行任何修改
-    .
-end
+# 将处理后的数据转换为字符串，并返回给客户端
+$done({ body: (. | tojson) });
